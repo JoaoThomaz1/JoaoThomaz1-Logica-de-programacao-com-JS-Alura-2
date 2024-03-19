57aaa413962717c74abe3e293562f0453f38723e
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2024-03-19 18:05:27 -0300

    commit aula 2

---
+++ Desafio2/app.js
@@ -0,0 +1,22 @@
+//exe 1
+
+function exibirTexto() {
+   console.log('Olá, mundo!');
+};
+
+//exe 2
+let nome = 'João';
+function exibirTexto2(nome) {
+   console.log(`Olá, ${nome}!`);
+};
+
+//exe 3
+
+
+
+//exe 4
+
+//exe 5
+
+//exe 6
+
